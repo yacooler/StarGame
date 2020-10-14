14f8579ed52ec3cd54d3ff53b07f9342d1e0d9be
yacooler <yacooler@yandex.ru>
2020-10-14 18:59:38 +0300

    Initial commit

---
+++ android/src/com/vyazankin/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vyazankin.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vyazankin.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
